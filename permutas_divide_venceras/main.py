5bad1b703b72c4322c256e9ee52fcda733ad26e3
Bryan Miguel Flores Garcia <bryan.flores3210@alumnos.udg.mx>
2025-10-18 17:29:37 -0600

    Implementa grafica de complejidad temporal en algoritmo de divide y venceras de permutas

--- permutas_divide_venceras/main.py
+++ permutas_divide_venceras/main.py
@@ -1,7 +1,11 @@
+import time
+import matplotlib.pyplot as plt
+tiempos=[0]
+tamanio=[0]
+
 def permutas_fuerza_bruta(lista):
-    global permutas
     # Caso base: si la lista tiene un solo elemento, es una permutación.
-    if len(lista) <= 1:
+    if len(lista) == 1:
         return [lista]
     permutas_totales=[]
     # Recorrer cada elemento de la lista
@@ -18,6 +22,30 @@ def permutas_fuerza_bruta(lista):
             permutas_totales.append(nueva_permutacion)
     return permutas_totales
 
-permutas = []
-lista = [1, 2, 3]   
-print(permutas_fuerza_bruta(lista)) 
+def medir_tiempo(funcion,lista):
+    inicio = time.perf_counter()
+    _ = funcion(lista)
+    fin = time.perf_counter()
+    tiempo_ms = (fin - inicio) * 1000
+    return  tiempo_ms
+
+def graficar():
+    global tiempos
+    global tamanio
+    plt.plot(tamanio, tiempos, marker='o', color='r', linestyle='-')
+    plt.title('Tiempos de ejecución de permutaciones por divide y vencerás')
+    plt.xlabel('Tamaño de la lista (n)')
+    plt.ylabel('Tiempo de ejecución (ms)')
+    plt.grid()
+    plt.show()
+    
+if __name__ == "__main__":
+    for n in range(1,12):
+        tiempos.append(medir_tiempo(permutas_fuerza_bruta,list(range(n))))
+        tamanio.append(n)
+        graficar()
+        print(f"Tamaño: {n}, Tiempo: {tiempos[-1]:.4f} ms")
+        input("Presiona Enter para continuar...")
+        
+        
+   
